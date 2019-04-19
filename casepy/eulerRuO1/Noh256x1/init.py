84a1efa0a726fdc2fed627fa69e2de15e1cdb955
William W <william.weens@gmail.com>
2019-04-19 23:28:57 +0200

    New way to code the Noh

---
+++ casepy/eulerRuO1/Noh256x1/init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import sys
+import os
+for p in sys.path:
+    if "eulerRuO1/Noh" in p:
+        sys.path.insert(0, os.path.join(p, '../'))
+        break
+
+from nohXx1 import *
