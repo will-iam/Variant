Display fixed for Noh and Sod, 1D and 2D, norm computation fixed

casepy/eulerRuO1/tNoh256x256/chars.py
@@ -1,10 +1,7 @@
 import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '../../../'))
 import script.rio as io
-#import script.initial_condition.noh2D as noh2D
-import numpy as np
-from decimal import Decimal
-from math import sqrt, sin
+import script.initial_condition.noh2D as noh2D
 
 # Domain properties
 lx = 1.0
@@ -21,79 +18,7 @@ gamma = 5./3.
 BClayer = 1
 
 quantityList = ['rho', 'rhou_x', 'rhou_y', 'rhoE']
+
 def buildme(quantityDict, coords_to_uid, coords_to_bc):
-    # ------------------------------------------------------------------------------
-    # Domain properties
-    # ------------------------------------------------------------------------------
-    dx = lx / Nx
-    dy = ly / Ny
+    noh2D.build(quantityDict, coords_to_uid, coords_to_bc, Nx, Ny, lx, ly, BClayer)
 
-    # ------------------------------------------------------------------------------
-    # rho initial state
-    # ------------------------------------------------------------------------------
-    rho_uid_to_val = np.zeros((Nx*Ny), dtype = np.dtype(Decimal))
-    rhou_x_uid_to_val = np.zeros((Nx*Ny), dtype = np.dtype(Decimal))
-    rhou_y_uid_to_val = np.zeros((Nx*Ny), dtype = np.dtype(Decimal))
-    rhoE_uid_to_val = np.zeros((Nx*Ny), dtype = np.dtype(Decimal))
-
-    u0=1.0
-    rho0=1.0
-
-    for i in range(Nx):
-        for j in range(Ny):
-            x = (i + 0.5) * dx
-            y = (j + 0.5) * dy
-            coords = coords_to_uid[(i, j)]
-            rho_uid_to_val[coords] = rho0
-            rhou_x_uid_to_val[coords] = -u0*x/sqrt(x**2+y**2)*rho0
-            rhou_y_uid_to_val[coords] = -u0*y/sqrt(x**2+y**2)*rho0
-            rhoE_uid_to_val[coords] = 1.0/2.0*u0**2*rho0          #Kinetic energy at t=0
-
-    # ------------------------------------------------------------------------------
-    # Boundary conditions
-    # ------------------------------------------------------------------------------
-    # Start new uid after last domain cell
-    for k in range(1, BClayer + 1):
-        # Left border
-        for j in range(-k, Ny - 1 + k):
-            coords_to_bc[(-k, j)] = {'N': {"rho": 1, "rhoE": 1, "pressure": 1, "rhou_x": -1, "rhou_y": 1}}
-
-        # Top border
-        for i in range(-k, Nx - 1 + k):
-            x = (i + 0.5) * dx
-            y = (Ny + 0.5) * dy
-            ir = 1. / np.sqrt(x**2 + y**2)
-            # rho_exact = rho0 * (1 + t * u0 / r) => (rho0, u0 / r)
-            # rhoE_exact = rho_exact * 0.5 * u0**2 = rho0 * (1 + t * u0 / r) * 0.5 * u0**2 => (rho0 * 0.5 * u0**2, u0 / r)
-            # rhou_x_exact = -u0 * rho_exact * x / r = rho0 * (1 + t * u0 / r) * (-u0 * x / r) => (-u0 * rho0 * x / r, u0 / r)
-            # rhou_y_exact = -u0 * rho_exact * y / r = rho0 * (1 + t * u0 / r) * (-u0 * y / r) => (-u0 * rho0 * y / r, u0 / r)
-            coords_to_bc[(i, Ny - 1 + k)] = {
-                'D': {"rho": rho0, "rhoE": 0.5 * rho0 * u0**2, "pressure": 0, "rhou_x": -u0 * rho0 * x * ir, "rhou_y": -u0 * rho0 * y * ir},
-                'T': {"rho": u0 * ir, "rhoE": u0 * ir, "rhou_x": u0 * ir, "rhou_y": u0 * ir}
-            }
-            #coords_to_bc[(i, Ny - 1 + k)] = {'D': {"rho": u0, "rhoE": 1/2*u0**2*rho0, "pressure": 0,"rhou_x": -u0*x/sqrt(x**2+y**2)*rho0, "rhou_y": -u0*y/sqrt(x**2+y**2)*rho0}}
-
-        # Right border
-        for j in range(Ny, -k, -1):
-            x = (Nx + 0.5) * dx
-            y = (j + 0.5) * dy
-            ir = 1. / np.sqrt(x**2 + y**2)
-            coords_to_bc[(Nx - 1 + k, j)] = {
-                'D': {"rho": rho0, "rhoE": 0.5 * rho0 * u0**2, "pressure": 0,"rhou_x": -u0 * rho0 * x * ir, "rhou_y": -u0 * rho0 * y * ir},
-                'T': {"rho": u0 * ir, "rhoE": u0 * ir, "rhou_x": u0 * ir, "rhou_y": u0 * ir}
-            }
-            #coords_to_bc[(Nx - 1 + k, j)] = {'D': {"rho": u0, "rhoE": 1/2*u0**2*rho0, "pressure": 0,"rhou_x": -u0*x/sqrt(x**2+y**2)*rho0, "rhou_y": -u0*y/sqrt(x**2+y**2)*rho0}}
-
-        # Bottom border
-        for i in range(Nx, -k, -1):
-            coords_to_bc[(i, -k)] = {'N': {"rho": 1, "rhoE": 1, "pressure": 1, "rhou_x": 1, "rhou_y": -1}}
-
-    # ------------------------------------------------------------------------------
-    # velocity states
-    # ------------------------------------------------------------------------------
-
-    # Add quantities to the quantity dictionary
-    quantityDict['rho'] = rho_uid_to_val
-    quantityDict['rhou_x'] = rhou_x_uid_to_val
-    quantityDict['rhou_y'] = rhou_y_uid_to_val
-    quantityDict['rhoE'] = rhoE_uid_to_val

script/error_norm.py
@@ -43,27 +43,34 @@ def compute(data_path, qty_name_list, solver):
     # Call solver.
     #in values: ['energy', 'p', 'u', 'rho', 'rho_total', 'x']
     start = timer()
-    gamma = 1.4
     if solver == 'sod':
         if Nx != 1 and Ny != 1:
             return None
         from sod import solve
         positions, regions, values = solve(left_state=(1, 1, 0), right_state=(0.1, 0.125, 0.),
-                        geometry=(0., 1., 0.5), t=0.2, gamma=gamma, npts=Nx)
+                        geometry=(0., 1., 0.5), t=0.2, gamma=1.4, npts=Nx)
     if solver == 'sedov':
         from sedov import solve
-        x = np.linspace(0., Nx * float(dx), Nx)
-        values = solve(t=1.0, gamma=gamma, xpos=x)
+        x = np.linspace(0., Nx * float(dx) * np.sqrt(2), Nx)
+        if Ny==1:
+            values = solve(t=1.0, gamma=1.4, xpos=x, ndim=1)
+        if Nx and Ny>1:
+            values = solve(t=1.0, gamma=1.4, xpos=x, ndim=2)
 
     if solver == 'noh':
         from noh import solve
-        x = np.linspace(0., Nx * float(dx), Nx)
+        if Nx==1:
+            values = solve(t=0.6, gamma=5./3., ndim=1,npts=Ny)
         if Ny==1:
             values = solve(t=0.6, gamma=5./3., ndim=1,npts=Nx)
         if Ny>1:
             values = solve(t=0.6, gamma=5./3., ndim=2,npts=Nx)
-    end = timer()
     
+    
+#    values['rho'] = 0*np.ones(Nx)
+
+
+    end = timer()
     print("Time to compute exact solution of %s elements" % len(values['rho']), "%s second(s)" % int((end - start)))
 
     result = {'N' : Nx*Ny}
@@ -85,7 +92,11 @@ def compute(data_path, qty_name_list, solver):
 
     end = timer()
     print("Time to load quantity rho of %s elements" % (Nx * Ny), "%s second(s)" % int((end - start)))
-    result["rho"] = np.linalg.norm((rho - values["rho"]), ord=2) / Nx
+    if (solver == 'sedov' or solver == 'noh') and (Nx>1 and Ny>1):      #2D Sedov or Noh
+        result["rho"] = np.linalg.norm((rho - values["rho"]), ord=2) * min(float(dx),float(dy)) * np.sqrt(2)
+    else:
+        result["rho"] = np.linalg.norm((rho - values["rho"]), ord=2) * min(float(dx),float(dy))
+
 
     '''
     q_map = {"rhoE" : "energy", "rhou_x" : "u"}

script/study/plot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import __future__
 from common import *
 import math
 
@@ -54,13 +53,17 @@ if axis == 'x':
             qty[i] = data['rhou_x'][i][0] / data['rho'][i][0]
     elif towatch == "energy":
         for i in range(Nx):
-            #qty[i] = (data['rhoE'][i][0]) / data['rho'][i][0]
-            qty[i] = data['rhoE'][i][0] / data['rho'][i][0] -(data['rhou_x'][i][0] / data['rho'][i][0])**2/2.0
+            ux = data['rhou_x'][i][0] / data['rho'][i][0]
+            Ek = ux**2/2.0
+            qty[i] = data['rhoE'][i][0] / data['rho'][i][0] - Ek
     elif towatch == "rho":
         for i in range(Nx):
             qty[i] = data['rho'][i][0]
 
 elif axis == 'y':
+    if Ny == 1:
+        print("Cannot show 1D case on axis y, change axis y to axis x")
+        sys.exit(1)
     axis_title="x = 0"
     Npoints = Ny
     x = np.linspace(0., Ny * float(dy), Ny)
@@ -70,54 +73,36 @@ elif axis == 'y':
             qty[i] = data['rhou_y'][0][i] / data['rho'][0][i]
     elif towatch == "energy":
         for i in range(Ny):
-            qty[i] = data['rhoE'][0][i] / data['rho'][0][i] -(data['rhou_y'][0][i] / data['rho'][0][i])**2/2.0
+            uy = data['rhou_y'][0][i] / data['rho'][0][i]
+            Ek = uy**2/2.0
+            qty[i] = data['rhoE'][0][i] / data['rho'][0][i] - Ek
     elif towatch == "rho":
         for i in range(Ny):
             qty[i] = data['rho'][0][i]
 
 elif axis == 'xy':
-    axis_title="y = x"
-    Npoints = math.floor(max(Nx,Ny)/np.sqrt(2.0))+1
-    x = np.linspace(0., max(Nx, Ny) * float(min(dx,dy)), Npoints)
-    #x = np.linspace(0., np.sqrt(2.0)*Ny * float(dy), Ny)
-    qtynormed = np.zeros(max(Nx,Ny))
-    if towatch == "u":
-        for i in range(max(Nx,Ny)):
-            uy = data['rhou_y'][i][i] / data['rho'][i][i]
-            ux = data['rhou_x'][i][i] / data['rho'][i][i]
-            qtynormed[i] = np.sqrt(ux**2. + uy**2) if ux>=0 and uy>=0 else -np.sqrt(ux**2+uy**2)
-    elif towatch == "energy":
-        for i in range(max(Nx,Ny)):
-            qtynormed[i] = data['rhoE'][i][i] / data['rho'][i][i] -(data['rhou_x'][i][i]**2 + data['rhou_y'][i][i]**2) / data['rho'][i][i]**2/2.0
-    elif towatch == "rho":
-        for i in range(max(Nx,Ny)):
-            qtynormed[i] = data['rho'][i][i]
-
-    if Nx > 1 and Ny > 1: #2D, Nx=Ny
-        qty = np.zeros(Npoints)
-        for i in range(len(qtynormed)):
-            if i * np.sqrt(2.0) <= 1.0 * len(qtynormed):
-                qty[i] = qtynormed[i]
-    else:      #1D
-        qty = qtynormed
-
-
-elif axis == 'r':
-    if Nx != Nx:
+    if Nx != Ny:
         print("Radial measure: domain must be a square Nx x Ny")
+        sys.exit(1)
+    if args.solver == 'sod':
+        print("Cannot compare with analytical solution on axis x=y, analytical solution not known")
         sys.exit(1)
     axis_title="diagonal"
     Npoints = Ny
-    x = np.linspace(0., Ny * float(dy), Ny)
+    x = np.linspace(0., Ny * float(dy) * np.sqrt(2), Ny)
     qty = np.zeros(Ny)
+    
     if towatch == "u":
         for i in range(Ny):
             uy = data['rhou_y'][i][i] / data['rho'][i][i]
             ux = data['rhou_x'][i][i] / data['rho'][i][i]
-            qty[i] = np.sqrt((ux**2. + uy**2.) / 2.)
+            qty[i] = np.sqrt(ux**2. + uy**2) if ux>=0 and uy>=0 else -np.sqrt(ux**2+uy**2)
     elif towatch == "energy":
         for i in range(Ny):
-            qty[i] = data['rhouE'][i][i] / data['rho'][i][i]
+            uy = data['rhou_y'][i][i] / data['rho'][i][i]
+            ux = data['rhou_x'][i][i] / data['rho'][i][i]
+            Ek = (uy**2 + ux**2)/2.0
+            qty[i] = data['rhoE'][i][i] / data['rho'][i][i] - Ek
     elif towatch == "rho":
         for i in range(Ny):
             qty[i] = data['rho'][i][i]
@@ -131,7 +116,10 @@ if args.solver == 'sod':
 if args.solver == 'sedov':
     sys.path.insert(1, os.path.join(sys.path[0], 'sedov'))
     from sedov import solve
-    values = solve(t=1.0, gamma=1.4, xpos=x)
+    if Ny==1:
+        values = solve(t=1.0, gamma=1.4, xpos=x, ndim=1)
+    if Nx and Ny>1:
+        values = solve(t=1.0, gamma=1.4, xpos=x, ndim=2)
 
 if args.solver == 'noh':
     sys.path.insert(1, os.path.join(sys.path[0], 'noh'))
@@ -141,12 +129,14 @@ if args.solver == 'noh':
     if Ny==1:
         values = solve(t=0.6, gamma=5./3., npts=Nx, ndim=1, axis='x')
     if Nx>1 and Ny>1:      #2D, Nx=Ny
-        if axis=='xy':
-            values = solve(t=0.6, gamma=5./3., npts=Npoints, ndim=2)
-        else:
-            values = solve(t=0.6, gamma=5./3., npts=Nx, ndim=2)
-print("intégrale exacte de",towatch," : ", sum(values[towatch]/max(Nx,Ny)))
-print("intégrale simulation de",towatch," : ", sum(qty/max(Nx,Ny)))
+        values = solve(t=0.6, gamma=5./3., npts=Npoints, ndim=2)
+
+if (args.solver == 'sedov' or args.solver == 'noh') and (Nx>1 and Ny>1):      #2D Sedov or Noh
+    print("intégrale simulation de",towatch," : ", sum(qty/max(Nx,Ny)) * Ny * float(dy) * np.sqrt(2)) 
+    print("intégrale exacte de",towatch," : ", sum(values[towatch]/max(Nx,Ny))* Ny * float(dy) * np.sqrt(2))
+else:
+    print("intégrale simulation de",towatch," : ", sum(qty/max(Nx,Ny)) * Ny * float(dy)) 
+    print("intégrale exacte de",towatch," : ", sum(values[towatch]/max(Nx,Ny))* Ny * float(dy))
 
 fig = plt.figure(0, figsize=(9, 6))
 ax1 = fig.add_subplot(111)
@@ -163,7 +153,7 @@ ax1.legend()
 
 color = 'red'
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-ax2.plot(x[0:511], np.abs(qty[0:511] - values[towatch][0:511]), linewidth=1.5, color=color, label="err.")
+ax2.plot(x, np.abs(qty - values[towatch]), linewidth=1.5, color=color, label="err.")
 ax2.set_ylabel('err.', color=color)  # we already handled the x-label with ax1
 ax2.tick_params(axis='y', labelcolor=color)
 

script/study/visualize.py
@@ -37,8 +37,12 @@ for q in quantityListName:
 
 x = np.linspace(0., Nx * float(dx), Nx)
 y = np.linspace(0., Ny * float(dy), Ny)
-Npoints = math.floor(max(Nx,Ny)/np.sqrt(2.0))+1
-x_1D = np.linspace(0., max(Nx,Ny) * float(min(dx,dy)), Npoints)
+Npoints = max(Nx,Ny)
+if args.solver == 'sod':
+    x_1D = np.linspace(0., Nx * float(dx), Nx)
+else:
+    x_1D = np.linspace(0., Nx * float(dx) * np.sqrt(2), Nx)
+
 Ux = np.zeros((Nx, Ny))
 Uy = np.zeros((Nx, Ny))
 Ek = np.zeros((Nx, Ny))
@@ -70,7 +74,12 @@ if args.solver == 'sedov':
     exact_title='Rho value on y = x'
     sys.path.insert(1, os.path.join(sys.path[0], 'sedov'))
     from sedov import solve
-    values = solve(t=1.2, gamma=1.4, xpos=x)
+
+
+    if Ny==1:
+        values = solve(t=1.0, gamma=1.4, xpos=x_1D, ndim=1)
+    if Nx and Ny>1:
+        values = solve(t=1.0, gamma=1.4, xpos=x_1D, ndim=2)
 
 if args.solver == 'noh':
     if Nx==1:
@@ -83,13 +92,7 @@ if args.solver == 'noh':
         exact_title='Rho value on y = 0'
     if Nx>1 and Ny>1:      #2D, with Nx=Ny
         for i in range(Nx):
-            rho_1D = np.zeros(math.floor(max(Nx,Ny)/np.sqrt(2.0))+1)
-            rho_1D_normed = np.zeros(max(Nx,Ny))
-            for i in range(Nx):
-                rho_1D_normed[i] = data['rho'][i][i]
-            for i in range(len(rho_1D_normed)):
-                if i/len(rho_1D_normed)<=1.0/np.sqrt(2.0):
-                    rho_1D[i] = rho_1D_normed[i]
+            rho_1D[i] = data['rho'][i][i]
         exact_title='Rho value on y = x'
     sys.path.insert(1, os.path.join(sys.path[0], 'noh'))
     from noh import solve
@@ -99,6 +102,7 @@ if args.solver == 'noh':
         values = solve(t=0.6, gamma=5./3., ndim=1,npts=Nx,axis='x')
     if Nx>1 and Ny>1:     #2D, with Nx=Ny
         values = solve(t=0.6, gamma=5./3., ndim=2,npts=Nx)
+
 print("intégrale de rho sur tout le domaine (simulation) : ", sum(sum(data['rho'])/(Nx*Ny)))
 #print("intégrale de rho exacte : ", sum(sum(values['rho'])/(Nx*Ny)))
 
@@ -120,7 +124,7 @@ elif Ny==1:
     ax1.plot(x, rho_1D, 'b+-', linewidth=2, markersize=3, label="simul.")
 else:
     ax1.plot(x_1D, rho_1D, 'b+-', linewidth=2, markersize=3, label="simul.")
-plt.plot(values['x'], values['rho'], linewidth=1.5, color='r', linestyle='dashed', label="exact")
+plt.plot(x_1D, values['rho'], linewidth=1.5, color='r', linestyle='dashed', label="exact")
 ax1.set(xlabel='x', ylabel='density', title=exact_title)
 ax1.grid()
 #ax1.axis([0, 1, 0, 1.1])